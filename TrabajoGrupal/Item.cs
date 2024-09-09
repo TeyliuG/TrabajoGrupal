a1cd25bd9516758e56f68df030a97485dac3867d
Marcelo Salerno <marcesbroxx@gmail.com>
2024-09-09 17:15:00 -0500

    Item Name, Price and Type

--- TrabajoGrupal/Item.cs
+++ TrabajoGrupal/Item.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace TrabajoGrupal
 {
+
     internal class Item
     {
+        private string name;
+        private int price;
+        private int type;
+        
+        public Item(string name,int price,int type)
+        {
+            this.name = name;
+            this.price = price;
+            this.type = type;
+
+        }
+
     }
 }
